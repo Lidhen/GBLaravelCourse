dbd7e5090a0f97454b15baf4fe9318f744d11e13
Ploskonos Lidiya <lidhen@yandex.ru>
2022-04-25 16:18:43 +0300

    add page Hello Word

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+$text = 'Привет мир!';
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$title = 'Моя первая страница';
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () use ($text, $title) {
+    return " 
+        <!doctype html>
+        <html lang=\"en\">
+        <head>
+            <meta charset=\"UFT-8\">
+            <title>$title</title>
+        </head>
+        <body>
+            <h1>$text</h1>
+            Lorem ipsum dolor sit amet.
+        </body>
+    ";
 });
